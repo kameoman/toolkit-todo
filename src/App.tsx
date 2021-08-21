29690fa5809145e766faed4c2b37097feedaa57b
kameoman <spiderman.935963@gmail.com>
2021-08-21 15:28:42 +0900

    UI構築

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Header from "./components/header/Header";
+import TaskForm from "./features/task/taskForm/TaskForm";
+import TaskList from "./features/task/taskList/TaskList";
 import styles from "./App.module.scss";
-import TaskForm from './features/task/taskForm/TaskForm';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <div className={styles.wrapper}>
         <Header />
         <TaskForm />
+        <TaskList />
       </div>
     </div>
   );
