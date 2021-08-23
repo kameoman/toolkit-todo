138d2f3bf8ed717e2651b7fb0d197543846e37e1
kameoman <spiderman.935963@gmail.com>
2021-08-23 17:42:46 +0900

    redux側実装

---
+++ src/features/task/taskSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState, AppThunk } from "../../app/store";
+import { useSelector } from "react-redux";
+
+interface TaskState {
+  // タスクの数量を管理する
+  idCount: number;
+  // storeに保存するタスクの一覧
+  tasks: { id: number; title: string; completed: boolean }[];
+  // taskのtitleを編集する際にどのtaskが選択されているかを知るため
+  selectedTask: { id: number; title: string; completed: boolean };
+  // モーダルの入力で開くか閉じるかを判定する
+  isModalOpen: boolean;
+}
+
+const initialState: TaskState = {
+  idCount: 1,
+  tasks: [{ id: 1, title: "Task A", completed: false }],
+  selectedTask: { id: 0, title: "", completed: false },
+  isModalOpen: false,
+};
+
+export const taskSlice = createSlice({
+  name: "task",
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    // taskの作成
+    createTask: (state, action) => {
+      state.idCount++;
+      const newTask = {
+        id: state.idCount,
+        title: action.payload,
+        completed: false,
+      };
+      state.tasks = [newTask, ...state.tasks];
+    },
+  },
+});
+
+export const { createTask } = taskSlice.actions;
+
+export const selectTask = (state: RootState): TaskState["tasks"] =>
+  state.task.tasks;
+// useSelector(selectTask)
+
+export default taskSlice.reducer;
